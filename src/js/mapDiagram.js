aed4e4d59efb6f5484e144c4a166643263271188
Andrei34 <andrei@planet34.org>
2016-10-18 18:39:54 +0300

    > Remove mapInfo content manipulation > Implemented onClick - show on each inactive country the number of active country diaspora > All countries are now generated in their own group to which number labels also belong

--- src/js/mapDiagram.js
+++ src/js/mapDiagram.js
@@ -3,7 +3,7 @@
       var center, countries, height, path, projection, scale, svg, width, mapData;
       width = 960;
       height = 600;
-      center = [18,63];
+      center = [15,63];
       scale = 750;
       projection = d3.geo.mercator().scale(scale).translate([width / 2, 0]).center(center);
       path = d3.geo.path().projection(projection);
@@ -24,15 +24,26 @@
         countries.selectAll(".country")
         .data(topojson.feature(data, data.objects.collection).features)
         .enter()
-        .append("path")
-        .attr("class","country")
-        .attr("d", path)
-        .attr("id", function (d) {return d.properties.NAME;})
-        .filter(function (d) {if (d.properties.EU_MEMBER === "1") {return d;}})
-        .attr("class", "countryEU")
-        .on("mouseover", mouseover)
-        .on("mouseout", mouseout)
-        .on("click", onClick);
+        .append("g")
+        .attr("class",function (d) {if (d.properties.EU_MEMBER === "1") {return "gCountryEU";}else {return "gCountry";}})
+        .attr("id", function (d) {return "g" + d.properties.NAME;})
+          .append("path")
+          .attr("class","country")
+          .attr("id", function (d) {return d.properties.NAME;})
+          .attr("d", path)
+          .filter(function (d) {if (d.properties.EU_MEMBER === "1") {return d;}})
+          .attr("class", "countryEU")
+          .on("mouseover", mouseover)
+          .on("mouseout", mouseout)
+          .on("click", onClick);
+
+        //MAKE TEXT CONTAINERS FOR COUNTRY NUMBERS
+        d3.selectAll(".gCountryEU").append("text")
+          .attr("class", "nrCountry selectDisallow")
+          .attr("id", function (d) {return "nr" + d.properties.NAME;})
+          .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+          .attr("dy", ".35em")
+          .style("opacity", 0);
 
       //IMPORT SECONDARY DIASPORA DATA AND ASSIGN TO COUNTRIES
       d3.json('./data/mapdata.json', function (data) {
@@ -40,6 +51,7 @@
 
         for (var key in diasporaNumbers){
           if (diasporaNumbers.hasOwnProperty(key)){
+
           //MAKE SELECTION AND UPDATE DATA
           d3.select("#" + key).datum(function(d){
             d.diaspora = diasporaNumbers[key]; 
@@ -84,13 +96,13 @@
       if (initialClass === "countryActive"){
         d3.select(this).attr("class", "countryEU");
         d3.selectAll(".countryEU").transition().duration(easingDuration).ease("cubic").style("fill", "#217A89");
-        d3.select("#mapInfo").text("Select a country to view information.");
+        d3.selectAll(".nrCountry").transition().duration(easingDuration).ease("cubic").style("opacity", 0);
         d3.select("#selectionTitle").transition().duration(easingDuration/2).ease("quad").style("fill", "#FFFFFF").remove();
       }else {
         //SET THIS AS ACTIVE COUNTRY
         d3.selectAll(".countryEU").transition().duration(easingDuration).ease("cubic").style("fill", "#217A89");
+        d3.selectAll(".nrCountry").transition().duration(easingDuration).ease("cubic").style("opacity", 0).text("");
         d3.select(".countryActive").attr("class","countryEU");
-        d3.select("#mapInfo").text("");
         d3.select("#selectionTitle").transition().duration(easingDuration/2).ease("quad").style("fill", "#FFFFFF").remove();
         d3.select(this).attr("class", "countryActive").transition().duration(easingDuration).ease("exp").style("fill","#F47F5E");
 
@@ -139,15 +151,7 @@
                             .range(['#6baed6','#3182bd','#08519c']);
 
         //DISPLAY DIASPORA TEXT AND GO FOR HEATMAP
-        var diasporaText = "Top destinations for citizens of " + activeCountry + " are: ";
         for (var i = 0; i < restEuData.length; i++) {
-
-          //SET HOW MANY COUNTRIES TO DISPLAY ON CLICK
-          if (i < 5){
-            diasporaText += restEuData[i].host + " (" + d3.format(",")(restEuData[i].people) + "), ";
-            d3.select("#mapInfo").text(diasporaText);
-          }
-
           //CALCULATE % OF DIASPORA IN CURRENT ITERATION
           var diasporaPercentage = ((restEuData[i].people / activeDiaspora) * 100).toFixed(2);
 
@@ -157,6 +161,12 @@
             .duration(easingDuration)
             .ease("cubic")
             .style("fill", color(diasporaPercentage));
+          d3.selectAll("#nr" + restEuData[i].host)
+            .transition()
+            .duration(easingDuration)
+            .ease("cubic")
+            .style("opacity", 1)
+            .text(d3.format(".2s")(restEuData[i].people));
         }
       }
     }
