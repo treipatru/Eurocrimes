9295feef59e648de471b89e0c9f22ef886b8e849
Andrei34 <andrei@planet34.org>
2016-10-22 12:19:18 +0300

    Chord is now responsive

--- src/js/chordDiagram.js
+++ src/js/chordDiagram.js
@@ -1,74 +1,97 @@
+//-----------------------------------------------------------------------------
+//
+// IMPRISONED DIASPORA CHORD
+//
+//-----------------------------------------------------------------------------
+
+
+
+
 
 function drawChordDiagram () {
-  //  CREATE DATA MATRIX AND MAP DATA
-  //---------------------------------------------------------------------------
-  d3.csv('data/chord-data.csv', function (error, data) {
-    var mpr = chordMpr(data);
+  
+  //DYNAMIC WIDTH
+  var margin = {top: 10, left: 25, right: 25, bottom: 10},
+      boxWidth = parseInt(d3.select('#chordDiagram').style('width')),
+      boxWidth = boxWidth - margin.left - margin.right,
+      boxRatio = 0.75,
+      boxHeight = boxWidth * boxRatio;
 
-    mpr
-    .addValuesToMap('isfrom')
-    .setFilter(function (row, a, b) {
-      return (row.isfrom === a.name && row.goesto === b.name);
-    })
-    .setAccessor(function (recs, a, b) {
-      if (!recs[0]) return 0;
-      return +recs[0].count;
+  d3.select(window).on("resize", resizeChord);
+
+  render ();
+
+
+  function render () {
+    //CREATE DATA MATRIX AND MAP DATA
+    //--------------------------------------
+    d3.csv('data/chord-data.csv', function (error, data) {
+      var mpr = chordMpr(data);
+
+      mpr.addValuesToMap('isfrom')
+         .setFilter(function (row, a, b) {
+          return (row.isfrom === a.name && row.goesto === b.name);
+        })
+         .setAccessor(function (recs, a, b) {
+          if (!recs[0]) return 0;
+          return +recs[0].count;
+        });
+
+      drawChords(mpr.getMatrix(), mpr.getMap());
     });
-    drawChords(mpr.getMatrix(), mpr.getMap());
-  });
 
-  //  DRAW THE CHORD DIAGRAM
-  //---------------------------------------------------------------------------
-  function drawChords (matrix, mmap) {
-    var w = 700, h = 700, r1 = h / 2, r0 = r1 - 100;
 
-    var fill = d3.scale.ordinal()
-        .domain(d3.range(28))
-        .range(["#dc448f", "#449a2b", "#8132ab", "#a469dc", "#3a7524", 
-                "#b542a8", "#449357", "#4d47af", "#a97a20", "#5c75e3",
-                "#d05b23", "#6282ca", "#ce3c37", "#318d76", "#d03f5f",
-                "#315f2b", "#9b2e6b", "#798842", "#724e95", "#5c5d1b",
-                "#b2659a", "#8b6a34", "#3e6ca1", "#b86d3e", "#8a394a",
-                "#8d3f1c", "#bc615d"]);
+    //  DRAW THE CHORD DIAGRAM
+    //---------------------------------------------------------------------------
+    function drawChords (matrix, mmap) {
+      var r1 = boxHeight / 2, r0 = r1 - 100;
 
-    var chord = d3.layout.chord()
-        .padding(.04)
-        .sortSubgroups(d3.descending)
-        .sortChords(d3.descending);
+      var fill = d3.scale
+                   .ordinal()
+                   .domain(d3.range(28))
+                   .range(
+                    ["#dc448f", "#449a2b", "#8132ab", "#a469dc", "#3a7524", "#b542a8", "#449357", "#4d47af", "#a97a20", "#5c75e3",
+                     "#d05b23", "#6282ca", "#ce3c37", "#318d76", "#d03f5f", "#315f2b", "#9b2e6b", "#798842", "#724e95", "#5c5d1b",
+                     "#b2659a", "#8b6a34", "#3e6ca1", "#b86d3e", "#8a394a", "#8d3f1c", "#bc615d"]);
 
-    var arc = d3.svg.arc()
-        .innerRadius(r0 + 10)
-        .outerRadius(r0 + 30);
+      var chord = d3.layout
+                    .chord()
+                    .padding(0.04)
+                    .sortSubgroups(d3.descending)
+                    .sortChords(d3.descending);
 
-    var svg = d3.select("#chordDiagram").append("svg:svg")
-        .attr("width", w)
-        .attr("height", h)
-        .style("display","block")
-        .style("margin","auto")
-      .append("svg:g")
-        .attr("id", "circle")
-        .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+      var arc = d3.svg
+                  .arc()
+                  .innerRadius(r0 + 10)
+                  .outerRadius(r0 + 30);
 
-        svg.append("circle")
-            .attr("r", r0 + 20);
+      var svg = d3.select("#chordDiagram").append("svg:svg")
+                  .attr("width", boxWidth)
+                  .attr("height", boxHeight)
+                  .attr("id","chordContainer")
+                  .style("display","block")
+                  .style("margin","auto")
+                .append("svg:g")
+                  .attr("id", "circle")
+                  .attr("transform", "translate(" + boxWidth / 2 + "," + boxHeight / 2 + ")");
 
-    var rdr = chordRdr(matrix, mmap);
-    chord.matrix(matrix);
+      svg.append("circle").attr("r", r0 + 20);
 
-    var g = svg.selectAll("g.group")
-        .data(chord.groups())
-      .enter().append("svg:g")
-        .attr("class", "group")
-        .on("mouseover", mouseOverGroup)
-        .on("mousemove", mouseMoveGroup)
-        .on("mouseout", mouseOutGroup)
-        .on("click", onClickGroup);
+      var rdr = chordRdr(matrix, mmap);
+      chord.matrix(matrix);
 
-    g.append("svg:path")
-        .style("fill", function(d) { return fill(d.index); })
-        .attr("d", arc);
+      var g = svg.selectAll("g.group")
+                 .data(chord.groups())
+               .enter().append("svg:g")
+                 .attr("class", "group")
+                 .on("mouseover", mouseOverGroup)
+                 .on("mousemove", mouseMoveGroup)
+                 .on("mouseout", mouseOutGroup)
+                 .on("click", onClickGroup);
 
-    g.append("svg:text")
+      g.append("svg:path").style("fill", function(d) { return fill(d.index); }).attr("d", arc);
+
+      g.append("svg:text")
         .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
         .attr("dy", ".35em")
         .attr("class", "selectDisallow")
@@ -81,15 +104,15 @@ function drawChordDiagram () {
         .text(function(d) { return rdr(d).gname; });
 
       var chordPaths = svg.selectAll("path.chord")
-            .data(chord.chords())
-          .enter().append("svg:path")
-            .attr("class", "chord")
-            .style("stroke", function(d) { return d3.rgb(fill(d.target.index)).darker(); })
-            .style("fill", function(d) { return fill(d.target.index); })
-            .attr("d", d3.svg.chord().radius(r0))
-            .on("mouseover", mouseOverChord)
-            .on("mousemove", mouseMoveChord)
-            .on("mouseout", mouseOutChord);
+                          .data(chord.chords())
+                          .enter().append("svg:path")
+                          .attr("class", "chord")
+                          .style("stroke", function(d) { return d3.rgb(fill(d.target.index)).darker(); })
+                          .style("fill", function(d) { return fill(d.target.index); })
+                          .attr("d", d3.svg.chord().radius(r0))
+                          .on("mouseover", mouseOverChord)
+                          .on("mousemove", mouseMoveChord)
+                          .on("mouseout", mouseOutChord);
 
       function chordTip (d) {
         var p = d3.format(".2%");
@@ -105,11 +128,12 @@ function drawChordDiagram () {
 
 
 
-  //  MOUSE EVENTS
-  //---------------------------------------------------------------------------
+      // MOUSE EVENTS
+      //-----------------------------------------------------------------------
 
 
-      //GROUPS
+      //GROUPS BEHAVIOR
+      //------------------------------------
       function mouseOverGroup(d, i) {
         d3.select("#tooltip")
           .style("opacity", 0)
@@ -138,8 +162,6 @@ function drawChordDiagram () {
       }
 
       function mouseOutGroup (d) {
-
-        //HIDE TOOLTIP
         d3.select("#tooltip").style("visibility", "hidden");
         d3.selectAll(".fade")
           .transition()
@@ -152,7 +174,13 @@ function drawChordDiagram () {
         d3.select(this).select("text").style("font-weight","500");
       }
 
-      //CHORDS
+      function onClickGroup (d, i) {
+        //Keep chord selected
+      }
+
+
+      //CHORDS BEHAVIOR
+      //------------------------------------
       function mouseOverChord (d) {
         //FILL SHAPE 100%
         d3.select(this)
@@ -184,88 +212,39 @@ function drawChordDiagram () {
         d3.select("#tooltip").
         style("visibility", "hidden");
       }
+    }
+  }//end render
+  
+  //REDRAW ON RESIZE
+  function resizeChord () {
+    // adjust things when the window size changes
+    boxWidth = parseInt(d3.select('#chordDiagram').style('width'));
+    boxHeight = boxWidth * boxRatio;
+    
+    //remove current
+    d3.select("#chordContainer").remove();
 
-      function onClickGroup (d, i) {
-
-      }
+    //update svg
+    render();
   }
+}//end drawChordDiagram
 
-//END CHORD DIAGRAM
+
+
+
+
 //-----------------------------------------------------------------------------
-}
+//
+// HELPER FUNCTIONS
+//
+//-----------------------------------------------------------------------------
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//*******************************************************************
-//  CHORD MAPPER 
-//*******************************************************************
+//CHORD MAPPER
+//-----------------------------------------------------------------------------
 function chordMpr (data) {
   var mpr = {}, mmap = {}, n = 0,
       matrix = [], filter, accessor;
@@ -315,9 +294,10 @@ function chordMpr (data) {
   }
   return mpr;
 }
-//*******************************************************************
-//  CHORD READER
-//*******************************************************************
+
+
+//CHORD READER
+//-----------------------------------------------------------------------------
 function chordRdr (matrix, mmap) {
   return function (d) {
     var i,j,s,t,g,m = {};
